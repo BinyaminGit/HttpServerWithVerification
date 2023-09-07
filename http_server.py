e5154a464f8e9592084bf9269e2893725bd5e9a0
BinyaminGit <142875690+BinyaminGit@users.noreply.github.com>
2023-09-07 21:47:14 +0300

    Update http_server.py

--- http_server.py
+++ http_server.py
@@ -27,7 +27,7 @@ class MyHandler(SimpleHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            self.wfile.write(b'Hello, NVIDIA DOCA SDK!')
+            self.wfile.write(b'Hello, Example!')
 
     def do_POST(self):
         if self.path == '/data':
